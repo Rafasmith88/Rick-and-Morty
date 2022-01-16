a64c0c0106a5bf14f9c3039f3b98de997a99d582
Rafael Smith <smithyoungflu@gmail.com>
2022-01-16 20:16:47 -0300

    Inserção de captura de informações elementos html manipuláveis

---
+++ script.js
@@ -0,0 +1,15 @@
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const condicao = document.querySelector('#status');
+
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
